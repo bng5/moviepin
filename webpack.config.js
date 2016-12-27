e2ff50eb388cac776fbdb042fe029e4fb3de3fb2
diablo <jaime@diablourbano.com>
2016-12-27 17:33:57 -0300

    configures devDependencies and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ],
+  
+  output: {
+    filename: 'moviepin.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
+  context: resolve(__dirname, 'src'),
+  devtool: 'source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, '.'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+};
