302e9f4465553985342514c3a692693d4acf04a9
diablo <jaime@diablourbano.com>
2017-01-06 01:38:02 -0300

    updates index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import Search from './components/search';
 import SignIn from './components/signin';
 import Join from './components/join';
+import Menu from './components/menu';
+import MovieCard from './components/movie-card';
+import MovieDetail from './components/movie-detail';
 
 const render = (component) => {
   ReactDOM.render(
