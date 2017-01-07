17596c3fc4fb45b3ce775da16afc2195d3f6b1c7
diablo <jaime@diablourbano.com>
2017-01-07 07:15:28 -0300

    refactors landing page styles

--- src/views/landing.js
+++ src/views/landing.js
@@ -5,36 +5,40 @@ import AccessMenu from '../components/access-menu';
 class Landing extends Component {
 
   render() {
-    const containerRow = 'container container--row';
+    const containerRow = 'container--flex container--flex--row';
 
     return (
-      <div className='container container--column'>
-        <div className={containerRow +
-                        ' container--align-right' +
-                        ' container--align-middle'}>
-          <AccessMenu/>
-        </div>
-
-        <div className={containerRow +
-                        ' container--priority' +
-                        ' container--align-center' +
-                        ' container--align-top'}>
-          <div className='logo logo--landing'>
-            <span className='logo__icon icon-pin3'></span>
-            <h1 className='logo__name'>
-              Movie<span className='logo__name--emphasis'>Pin</span>
-            </h1>
+      <div className='container'>
+        <div className={'container--full-screen' +
+                        ' container--flex' +
+                        ' container--flex--column'}>
+          <div className={containerRow +
+                          ' container--flex--align-right' +
+                          ' container--flex--align-middle'}>
+            <AccessMenu/>
           </div>
-        </div>
 
-        <div className={containerRow + 
-                        ' container--align-center' +
-                        ' container--align-top'}>
-          <h2 className='container__headline container__headline--landing'>
-            movies
-            <span className='container__headline--emphasis'> worth </span>
-            to watch<span className='container__headline--emphasis'>!</span>
-          </h2>
+          <div className={containerRow +
+                          ' container--flex--priority' +
+                          ' container--flex--align-center' +
+                          ' container--flex--align-top'}>
+            <div className='logo logo--landing'>
+              <span className='logo__icon icon-pin3'></span>
+              <h1 className='logo__name'>
+                Movie<span className='logo__name--emphasis'>Pin</span>
+              </h1>
+            </div>
+          </div>
+
+          <div className={containerRow + 
+                          ' container--flex--align-center' +
+                          ' container--flex--align-top'}>
+            <h2 className='container__headline container__headline--landing'>
+              movies
+              <span className='container__headline--emphasis'> worth </span>
+              to watch<span className='container__headline--emphasis'>!</span>
+            </h2>
+          </div>
         </div>
       </div>
     );
