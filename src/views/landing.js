286b6e6c05667bcd8e9773a427b0f6be4b67a4b5
diablo <jaime@diablourbano.com>
2017-01-09 16:41:41 -0300

    few adjustments to landing view

--- src/views/landing.js
+++ src/views/landing.js
@@ -31,7 +31,6 @@ class Landing extends Component {
     if (this.state.displayOverlay) {
       this.setState({
         overlayClass: 'content-push--hidden',
-        formToShow: '',
         displayOverlay: false
       });
     }
